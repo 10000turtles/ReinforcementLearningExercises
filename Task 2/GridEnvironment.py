398ed850953c7548181af6d46a125659b095fa23
Turtles <10000turtles@gmail.com>
2019-07-20 16:22:14 -0400

    Added 2nd Task

---
+++ Task 2/GridEnvironment.py
@@ -0,0 +1,49 @@
+# Hello
+
+# Belman EQ: Value(move,state) = Max(Reward + WeightedSum(All possible outcomes))
+
+
+class GridEnvironment:
+  RewardKey = {"_": 0, "W": 1, "L": -10, "0": 0}
+  MoveKey = ["R", "U", "L", "D", "■"]
+  DirectionKey = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+
+  def __init__(self, envData, agentPos, obeyProbs, gamma):
+    self.board = envData  # envData is a 2-D array of characters/strings. _ = empty, 0 = wall, W = winning spot, L = losing spot.
+    self.boardValues = [[0 for i in envData[0]]for j in envData]  # initalizing a matrix the size of envData with all 0's
+    self.boardMoves = [[4 for i in envData[0]]for j in envData]  # Same thing
+    self.position = agentPos  # agentPos is a tuple of 2 integers representing where the player starts.
+    self.obeyProbs = obeyProbs  # obeyProbs is a list of 4 probabilities that add to 1 and explain the probability of the agent obeying. index 0 = Obey, index 1 = left, index 2 = back, index 3 = right.
+    self.gamma = gamma
+
+  def move(self, direction):  # direction is 0-3. 0 is right, 1 is up, 2 is left, 3 is down.
+    newSpace = (self.position[0]+GridEnvironment.DirectionKey[direction[0]], self.position[1]+GridEnvironment.DirectionKey[direction[1]])
+    if self.board[newSpace[0]][newSpace[1]] != "0":
+      self.position = newSpace
+
+  def printBoard(self):
+    for i in self.board:
+      print("-"*(len(self.board[0])*2+1))
+      a = "|"
+      for j in i:
+        a += j + "|"
+      print(a)
+    print("-"*(len(self.board[0])*2+1))
+
+  def printValues(self):
+    for i in self.boardValues:
+      print("-"*(len(self.board[0])*2+1))
+      a = "|"
+      for j in i:
+        a += str(j) + "|"
+      print(a)
+    print("-"*(len(self.board[0])*2+1))
+
+  def printMoves(self):
+    for i in self.boardMoves:
+      print("-"*(len(self.board[0])*2+1))
+      a = "|"
+      for j in i:
+        a += GridEnvironment.MoveKey[j] + "|"
+      print(a)
+    print("-"*(len(self.board[0])*2+1))
